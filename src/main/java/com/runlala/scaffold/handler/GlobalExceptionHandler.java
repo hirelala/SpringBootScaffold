b6afe37e6cf7331ac4f0c4c8ff3de9311dcb7c44
Quanuanc <quanuanc@outlook.com>
2023-12-31 14:12:11 +0800

    Fix "Call to 'printStackTrace()' should probably be replaced with more robust logging" warning

--- src/main/java/com/runlala/scaffold/handler/GlobalExceptionHandler.java
+++ src/main/java/com/runlala/scaffold/handler/GlobalExceptionHandler.java
@@ -2,18 +2,20 @@ package com.runlala.scaffold.handler;
 
 import com.runlala.scaffold.dto.R;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
     public R<String> handleIllegalArgumentException(IllegalArgumentException e, HttpServletRequest request) {
         String requestUrl = request.getRequestURL().toString();
         if (requestUrl.contains("/api")) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             return R.error(e.getMessage());
         }
 
