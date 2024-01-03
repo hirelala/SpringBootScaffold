94de5b228ba16445edd97da2307749180974c314
yeszao <guozao.m@gmail.com>
2024-01-03 17:25:06 +0400

    Remove @SpringBootTest to avoid spring connect to db while building

--- src/test/java/com/runlala/scaffold/ScaffoldApplicationTests.java
+++ src/test/java/com/runlala/scaffold/ScaffoldApplicationTests.java
@@ -2,8 +2,8 @@ package com.runlala.scaffold;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 class ScaffoldApplicationTests {
 
 	@Test
