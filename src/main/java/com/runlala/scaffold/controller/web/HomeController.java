29414261d8fea5e8dd19d52859be8feef94ff59e
yeszao <guozao.m@gmail.com>
2024-01-08 10:19:52 +0400

    Move /health to web module

--- src/main/java/com/runlala/scaffold/controller/web/HomeController.java
+++ src/main/java/com/runlala/scaffold/controller/web/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +13,11 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("greeting", "Hello SpringBootScaffold!");
         return "index";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String health() {
+        return "ok";
     }
 }
